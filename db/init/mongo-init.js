c5ee13f2248f589507056a539600ad334cc3377c
Jyri <105288995+jyrihkl@users.noreply.github.com>
2025-04-02 19:03:44 +0300

    Add super-linter workflows (#137)
    * Add super-linter workflow
    
    * Configure linter & add fix mode job
    
    * Try limiting running linters in fix job
    
    * Disable unfixable eslint warning
    
    * Add continue-on-error to fix job
    
    * Swap action to manual pushing
    
    * Remove unnecessary checkout
    
    * Maybe checkout earlier
    
    * Try cpr action
    
    * Add ref to checkout
    
    * Try manual once more
    
    * Add token
    
    * One more try with manual
    
    * Give up and run fixes only on push
    
    * Fix formatting
    
    * Swap to CPR action instead of auto-commit
    
    * Format workflow file

--- db/init/mongo-init.js
+++ db/init/mongo-init.js
@@ -1,5 +1,10 @@
-const user = fs.readFileSync(process.env.MONGODB_USERNAME_FILE, 'utf8').trim();
-const password = fs.readFileSync(process.env.MONGODB_PASSWORD_FILE, 'utf8').trim();
+/* eslint-disable no-undef */
+// This script is run in an environment where Mongo etc. are defined, but eslint doesn't know that
+
+const user = fs.readFileSync(process.env.MONGODB_USERNAME_FILE, "utf8").trim();
+const password = fs
+  .readFileSync(process.env.MONGODB_PASSWORD_FILE, "utf8")
+  .trim();
 
 // Create connection to mongo
 const conn = new Mongo();
@@ -7,8 +12,8 @@ const db = conn.getDB(process.env.MONGODB_INITDB_DATABASE);
 
 // Auth as the root user
 db.auth({
-    user: process.env.MONGODB_INITDB_ROOT_USERNAME,
-    pwd: process.env.MONGODB_INITDB_ROOT_PASSWORD
+  user: process.env.MONGODB_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGODB_INITDB_ROOT_PASSWORD,
 });
 
 // Create the user
@@ -17,18 +22,18 @@ db.createUser({
   pwd: password,
   roles: [
     {
-      role: 'readWrite',
-      db: process.env.MONGODB_DATABASE
-    }
-  ]
+      role: "readWrite",
+      db: process.env.MONGODB_DATABASE,
+    },
+  ],
 });
 
 // Create new database
 const newDb = db.getSiblingDB(process.env.MONGODB_DATABASE);
 
 // Create collections
-newDb.createCollection('students');
-newDb.createCollection('projects');
+newDb.createCollection("students");
+newDb.createCollection("projects");
 
 // Create indices
 // TODO: Add indices
