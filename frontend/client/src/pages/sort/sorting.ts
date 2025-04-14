87ef22788cb3288dd83f37c45fd3d60204761591
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2025-04-15 01:50:24 +0300

    sortFunc -> createStudentSorter

--- frontend/client/src/pages/sort/sorting.ts
+++ frontend/client/src/pages/sort/sorting.ts
@@ -2,36 +2,22 @@
 import { SortMethod, StudentSorter } from "../../types/SortMethods";
 import { StudentWithRow } from "../../types/Student";
 
-export const sortFunc = (
-  method: SortMethod,
-  reverse: boolean = false,
-): StudentSorter => {
-  const sortingFunctions = new Map<SortMethod, StudentSorter>([
-    ["default", (a: StudentWithRow, b: StudentWithRow) => a.row - b.row],
-    [
-      "name",
-      (a: StudentWithRow, b: StudentWithRow) =>
-        a.student.name.localeCompare(b.student.name) * (reverse ? -1 : 1),
-    ],
-    [
-      "score",
-      (a: StudentWithRow, b: StudentWithRow) => {
-        if (a.student.score === undefined || b.student.score === undefined) {
-          return a.student.name.localeCompare(b.student.name) * (reverse ? -1 : 1);
-        }
-        return a.student.score.score - b.student.score.score;
-      }
-    ],
-    [
-      "motivation",
-      (a: StudentWithRow, b: StudentWithRow) => {
-        if (a.student.score === undefined || b.student.score === undefined) {
-          return a.student.name.localeCompare(b.student.name) * (reverse ? -1 : 1);
-        }
-        return a.student.score.motivation - b.student.score.motivation;
-      }
-    ],
-  ]);
 
-  return sortingFunctions.get(method)!;
+export const createStudentSorter = (method: SortMethod, reverse: boolean = false): StudentSorter => {
+  const compareByNames = (a: StudentWithRow, b: StudentWithRow) => a.student.name.localeCompare(b.student.name) * (reverse ? -1 : 1)
+
+  const sortingFunctions = {
+    "default": (a: StudentWithRow, b: StudentWithRow) => a.row - b.row,
+    "name": compareByNames,
+    "score": (a: StudentWithRow, b: StudentWithRow) => {
+      if (a.student.score === undefined || b.student.score === undefined) return compareByNames(a, b);
+      return (a.student.score.score - b.student.score.score) * (reverse ? -1 : 1);
+    },
+    "motivation": (a: StudentWithRow, b: StudentWithRow) => {
+      if (a.student.score === undefined || b.student.score === undefined) return compareByNames(a, b);
+      return (a.student.score.motivation - b.student.score.motivation) * (reverse ? -1 : 1);
+    },
+  };
+
+  return sortingFunctions[method];
 };
