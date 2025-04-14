c951f3b2a0bb1ef1cd8dcc78ceabb8f55722f4be
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2025-04-15 01:56:13 +0300

    Remove Project type from here

--- frontend/client/src/services/project/default-projects.ts
+++ frontend/client/src/services/project/default-projects.ts
@@ -1,11 +1,5 @@
-export type Project = {
-  id: number;
-  name: string;
-  description: string;
-  batchesIds: number[];
-  tags: string[];
-  themes: string[];
-};
+/* Types */
+import { Project } from "../../types/Project";
 
 export const defaultProjects: Project[] = [
   {
