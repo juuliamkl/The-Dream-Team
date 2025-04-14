1d097fd7302492a2aacfc71917470b0cdf9e7700
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2025-04-14 22:20:43 +0300

    Fix score styling to account for no score data

--- frontend/client/src/components/score/score.component.tsx
+++ frontend/client/src/components/score/score.component.tsx
@@ -16,13 +16,20 @@ const Score = ({ score }: ScoreProps) => {
     const hueMax = 95;
 
     return (
-        (import.meta.env.MODE === "development") || score !== undefined ?
         <div className="score-label">
-            <span className="score" style={{"backgroundColor": `hsl(${(hueMax-hueMin) * dispScore! / 100 + hueMin}, 95%, 50%)`}}>{ dispScore }%
-            </span>
-            <span className="motivation" style={{"backgroundColor": `hsl(360, 0%, ${25 * dispMotivation! / 100 + 75}%)`}}>{ dispMotivation }%</span>
+            {
+                (import.meta.env.MODE === "development") || score !== undefined ?
+                <>
+                    <span className="score" style={{"backgroundColor": `hsl(${(hueMax-hueMin) * dispScore! / 100 + hueMin}, 95%, 50%)`}}>
+                        { dispScore }%
+                    </span>
+                    <span className="motivation" style={{"backgroundColor": `hsl(360, 0%, ${25 * dispMotivation! / 100 + 75}%)`}}>
+                        { dispMotivation }%
+                    </span>
+                </>
+                : <span className="loading">Loading Score...</span>
+            }
         </div>
-        : <span>Loading Score</span>
     )
 }
 
